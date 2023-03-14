1d91a780eaf3ec816b6dff6f1a357016436639e8
FiodorG <gre_dorelus@yahoo.com>
2023-03-14 18:48:13 -0400

    Added a method for US123

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Test");
+    }
+
+    // for US123
+    public static void add(int num1, int num2){
+        System.out.println(num1+num2);
     }
 }
